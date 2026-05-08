8328e46c145a0f8df8b7477e4b34bb3be17f7998
SwethaPodhila <venkannapodhila@gmail.com>
2026-05-08 14:22:45 +0530

    cors issue fixed

--- app.js
+++ app.js
@@ -9,7 +9,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:3000",
+            "https://corpfinder-frontend.onrender.com"
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    })
+);
 
 // 🔥 1. WEBHOOK FIRST (RAW BODY)
 app.post(
