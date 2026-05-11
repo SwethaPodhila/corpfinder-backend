b75c0003b333af6a371b1bab44a405fdcf225264
SwethaPodhila <venkannapodhila@gmail.com>
2026-05-11 11:31:55 +0530

    changed

--- routes/employeesCompanies.routes.js
+++ routes/employeesCompanies.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const path = require("path");
 
 // ✅ controller import
 const {
@@ -13,22 +14,55 @@ const {
 } = require("../controllers/employeecompanies.controller");
 
 // ✅ middleware import
-const { verifyAdmin,verifyUser } = require("../middleware/auth");
+const { verifyAdmin, verifyUser } = require("../middleware/auth");
 
 const router = express.Router();
 
-// 🔹 Multer config
-const upload = multer({
-    storage: multer.memoryStorage()
+
+// ✅ DISK STORAGE
+const storage = multer.diskStorage({
+
+    destination: function (req, file, cb) {
+        cb(null, "uploads/");
+    },
+
+    filename: function (req, file, cb) {
+
+        const uniqueName =
+            Date.now() + path.extname(file.originalname);
+
+        cb(null, uniqueName);
+    }
 });
 
+
+// ✅ multer config
+const upload = multer({
+    storage,
+
+    limits: {
+        fileSize: 20 * 1024 * 1024 // 20MB
+    }
+});
+
+
 router.post("/add", verifyAdmin, addEmployee);
-router.post("/upload", verifyAdmin, upload.single("file"), uploadEmployees);
+
+router.post(
+    "/upload",
+    verifyAdmin,
+    upload.single("file"),
+    uploadEmployees
+);
 
 router.get("/allEmployees", verifyAdmin, getEmployees);
+
 router.get("/:id", verifyAdmin, getEmployeeById);
+
 router.put("/update/:id", verifyAdmin, updateEmployee);
+
 router.delete("/delete/:id", verifyAdmin, deleteEmployee);
+
 router.get("/user/:id", verifyUser, getEmployeeByIdUser);
 
 module.exports = router;
