1e77268c8d5dec939b17f26be2269308af6673c3
SwethaPodhila <venkannapodhila@gmail.com>
2026-06-17 11:01:41 +0530

    changed

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { createSubscription } = require("../controllers/paymentController12.js");
+const { createOrder } = require("../controllers/payment.controller");
 const { verifyUser } = require("../middleware/auth");
 
 // ✅ normal API
-router.post("/create-order", verifyUser, createSubscription);
+router.post("/create-order", verifyUser, createOrder);
 
 // 🔥 FIXED webhook route
 //router.post("/webhook", cashfreeWebhook);
